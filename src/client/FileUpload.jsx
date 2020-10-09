29eb140988607fd34a0dc74538e11fa8cce16aa2
Brett Goss <goss.brett@gmail.com>
2020-10-09 13:58:47 -0700

    Create FileUpload component

---
+++ src/client/FileUpload.jsx
@@ -0,0 +1,95 @@
+import React, { useState, useRef } from 'react';
+
+export default function FileUpload({
+	handleFileUpload,
+	errorText = 'Invalid file type',
+	mainText = 'Choose a file',
+	acceptedTypes = ['text/csv'],
+}) {
+	const fileUploadInput = useRef();
+	const [dragging, setDragging] = useState(false);
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(false);
+	const [fileName, setFileName] = useState();
+
+	function getFile(event) {
+		if (event.target?.files?.length) {
+			return event.target.files[0];
+		} else if (event.dataTransfer?.files?.length) {
+			return event.dataTransfer.files[0];
+		}
+	}
+
+	async function handleUpload(event) {
+		event.preventDefault();
+		const file = getFile(event);
+		setDragging(false);
+
+		if (!file) {
+			return;
+		}
+		if (isValidFile(file)) {
+			await uploadFile(file);
+		} else {
+			setError(true);
+		}
+	}
+
+	async function uploadFile(file) {
+		try {
+			setLoading(true);
+			setError(false);
+			await handleFileUpload(file);
+			setFileName(file.name);
+		} catch (e) {
+			console.log('An error occurred while uploading file')
+			setError(true);
+		}
+		setLoading(false);
+	}
+
+	function isValidFile(file) {
+		return !!file && !!file.type && acceptedTypes.includes(file.type);
+	}
+
+	return (
+		<div>
+			<div
+				className="file has-name"
+				onDrop={async (event) => await handleUpload(event)}
+				onDragEnter={() => {
+					setDragging(true);
+				}}
+				onDragLeave={() => {
+					setDragging(false);
+				}}
+				onDragOver={(event) => {
+					event.preventDefault();
+				}}
+				onClick={() => {
+					fileUploadInput.current.click();
+				}}
+			>
+				<label className="file-label">
+					<span className="file-cta">
+						<span className="file-icon">
+							<i className="fas fa-upload"></i>
+						</span>
+						<span className="file-label">{mainText}</span>
+					</span>
+					{fileName && <span className="file-name">{fileName}</span>}
+				</label>
+			</div>
+			<input
+				id="FileUpload"
+				type="file"
+				className="is-hidden"
+				ref={fileUploadInput}
+				onChange={(event) => handleUpload(event)}
+				accept={acceptedTypes.toString()}
+				disabled={loading}
+			/>
+			{error && <div className="is-error">{errorText}</div>}
+		</div>
+	);
+}
