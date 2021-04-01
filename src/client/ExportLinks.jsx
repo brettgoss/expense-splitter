2d28064164960cb6cfd7f5f2847851f590e00ede
Brett Goss <goss.brett@gmail.com>
2021-04-01 08:28:49 -0700

    Make Mint export by month links dynamic

--- src/client/ExportLinks.jsx
+++ src/client/ExportLinks.jsx
@@ -1,45 +1,53 @@
 import React from 'react';
+import {
+	format,
+	startOfMonth,
+	endOfMonth,
+	addMonths,
+	subMonths,
+} from 'date-fns';
 
 const baseUrl = 'https://mint.intuit.com/transaction.event';
 // const baseExportUrl = 'https://mint.intuit.com/transactionDownload.event';
 
-const septemberTransactions = `?startDate=09/1/2020&endDate=09/30/2020&exclHidden=T`;
-const octoberTransactions = `?startDate=10/1/2020&endDate=10/31/2020&exclHidden=T`;
-const novemberTransactions = `?startDate=11/1/2020&endDate=10/30/2020&exclHidden=T`;
-
 export default function ExportLinks() {
-    return (
+	return (
 		<div className="buttons">
-			<a
-				className="button is-link"
-				href={`${baseUrl}${septemberTransactions}`}
-				target="_blank" rel="noreferrer"
-			>
-				<span>Mint - September 2020</span>
-				<span className="icon">
-					<i className="fas fa-external-link-alt"></i>
-				</span>
-			</a>
-			<a
-				className="button is-link"
-				href={`${baseUrl}${octoberTransactions}`}
-				target="_blank" rel="noreferrer"
-			>
-				<span>Mint - October 2020</span>
-				<span className="icon">
-					<i className="fas fa-external-link-alt"></i>
-				</span>
-			</a>
-			<a
-				className="button is-link"
-				href={`${baseUrl}${novemberTransactions}`}
-				target="_blank" rel="noreferrer"
-			>
-				<span>Mint - November 2020</span>
-				<span className="icon">
-					<i className="fas fa-external-link-alt"></i>
-				</span>
-			</a>
+			<ExportLink
+				startDate={startOfMonth(subMonths(new Date(), 1))}
+				endDate={endOfMonth(subMonths(new Date(), 1))}
+			/>
+			<ExportLink
+				startDate={startOfMonth(new Date())}
+				endDate={endOfMonth(new Date())}
+			/>
+
+			<ExportLink
+				startDate={startOfMonth(addMonths(new Date(), 1))}
+				endDate={endOfMonth(addMonths(new Date(), 1))}
+			/>
 		</div>
+	);
+}
+
+function ExportLink({ startDate, endDate }) {
+	function dateForMint(date) {
+		return format(new Date(date), 'MM/d/yyyy');
+	}
+
+	return (
+		<a
+			className="button is-link"
+			href={`${baseUrl}?startDate=${dateForMint(
+				startDate,
+			)}&endDate=${dateForMint(endDate)}&exclHidden=T`}
+			target="_blank"
+			rel="noreferrer"
+		>
+			<span>Mint - {format(new Date(startDate), 'LLLL yyyy')}</span>
+			<span className="icon">
+				<i className="fas fa-external-link-alt"></i>
+			</span>
+		</a>
 	);
 }
