d789c1e12c5bfe5e30ee8670d3b7da08b009f4e2
Brett Goss <goss.brett@gmail.com>
2022-04-23 17:38:13 -0700

    Upgrade all packages to latest

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import AppContainer from './AppContainer';
 
-ReactDOM.render(<AppContainer />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<AppContainer />);
